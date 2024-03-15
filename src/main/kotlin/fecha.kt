c6dedaefa440ab4f81bee0063d249c649d679951
Isabel Estevez <isabell.estevezz@gmail.com>
2024-03-15 22:11:01 +0100

    Crear funcion diaMes

--- src/main/kotlin/fecha.kt
+++ src/main/kotlin/fecha.kt
@@ -9,12 +9,17 @@ class Fecha(private val d: Int, private val m: Int, private val a: Int) {
         if (m < 1 || m > 12) return false
 
         // determinamos la cantidad de días del mes:
-        var diasMes = 0
+        var diasMes = diaMes()
         when (m) {
             1, 3, 5, 7, 8, 10, 12 -> diasMes = 31
             4, 6, 9, 11 -> diasMes = 30
-            2 -> diasMes = if (a % 400 == 0 || a % 4 == 0 && a % 100 != 0) 29 else 28
+            2 -> diasMes = if (a % 400 == diaMes() || a % 4 == diaMes() && a % 100 != diaMes()) 29 else 28
         }
         return if (d > diasMes) false else true
     } // … más métodos
+
+    private fun diaMes(): Int {
+        var diasMes = 0
+        return diasMes
+    }
 }
